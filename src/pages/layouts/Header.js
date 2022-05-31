f9b371fe171194cd4183b50e3f2abb8882334d2c
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2022-05-31 13:41:28 +1000

    layout created

--- src/pages/layouts/Header.js
+++ src/pages/layouts/Header.js
@@ -1,5 +1,20 @@
-import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Navbar bg="secondary" expand="md">
+        <Container>
+          <Navbar.Brand href="#home">My Toys</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Register</Nav.Link>
+              <Nav.Link href="#link">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
